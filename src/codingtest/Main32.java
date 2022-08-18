1ef6c3628cae0169ec844f14a18320d652650dcd
KimDanBee <eksql8061@naver.com>
2022-08-18 20:46:05 +0900

    2022-08-18

--- src/codingtest/Main32.java
+++ src/codingtest/Main32.java
@@ -1,10 +1,37 @@
 package codingtest;
 
+import java.util.Iterator;
+
 public class Main32 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		boolean[] bl = new boolean[10001];
+		
+		for (int i = 1; i < bl.length; i++) {
+			int a=d(i);
+			if (a<10001) {
+				bl[a] = true;
+			}
+		}
+		for (int i = 1; i < bl.length; i++) {
+			
+			if (bl[i]==false) {
+				System.out.println(i);
+			}
+		}
+	
+	}
+		
+	private static int d(int i) {
+		// TODO Auto-generated method stub
+		int sum = i;
+		
+		while (i > 0) {//33
+			sum += i%10;//3
+			i = i/10; //3
+		}
+		return sum;
 	}
 
 }
