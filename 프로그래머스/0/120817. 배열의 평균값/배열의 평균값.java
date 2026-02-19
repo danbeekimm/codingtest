f213b6c55a0cfc89f79adc71bc3d004cdce19001
KimDanBee <eksql8061@naver.com>
2026-02-19 10:28:48 +0900

    [level 0] Title: 배열의 평균값, Time: 0.04 ms, Memory: 83.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+
+class Solution {
+    public double solution(int[] numbers) {
+        double answer =0;
+        double sum =0;
+        
+        for(int num : numbers){
+            sum += num;
+        }
+        answer = sum / numbers.length;
+        return answer;
+    }
+}
