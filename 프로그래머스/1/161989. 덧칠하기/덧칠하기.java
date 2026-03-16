e47136e8b3a20403e2c0eec9b9d2ec0230462745
KimDanBee <eksql8061@naver.com>
2026-03-16 11:16:51 +0900

    [level 1] Title: 덧칠하기, Time: 1.85 ms, Memory: 91.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,18 @@
+    class Solution {
+        public int solution(int n, int m, int[] section) {
+            //n이 벽 길이 m 이 롤러길이 section이 칠해야할 곳
+            //2
+            int start = section[0];
+            int answer = 0;
+            //2,3,6
+            for (int item : section) {
+                if(item >= start){
+                    answer ++;
+                    start = item + m;
+                } else if (start > item){
+                    continue;
+                } 
+            }
+            return answer;
+        }
+    }
